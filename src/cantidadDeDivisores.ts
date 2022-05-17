cb25b3f683888db09a76182f71de7a83a77d328a
jorge-p-rolon <104174184+jorge-p-rolon@users.noreply.github.com>
2022-05-16 23:16:54 -0300

    nuevoEjercicio

---
+++ src/cantidadDeDivisores.ts
@@ -0,0 +1,26 @@
+//ejercicio cantidad de divisores
+//ROLON jORGE
+
+function esMultiplo(dividendo: number, divisor: number): boolean {
+  if (dividendo % divisor === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function cantidadDeDivisores(num: number): Number {
+  let multiplo: number = 0;
+  for (let cant: number = 0; cant <= num; cant++) {
+    if (esMultiplo(num, cant)) {
+      multiplo++;
+    }
+  }
+  return multiplo;
+}
+
+let numero: number = Number(prompt("ingrese numero:  "));
+console.log(
+  "La cantidad de multiplos de " + numero + " es : ",
+  cantidadDeDivisores(numero)
+);
