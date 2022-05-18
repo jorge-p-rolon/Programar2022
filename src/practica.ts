3a591989318f91a33b8f9b9c2dde0942c9ddc7f9
jorge-p-rolon <104174184+jorge-p-rolon@users.noreply.github.com>
2022-05-18 20:42:18 -0300

    practicando

---
+++ src/practica.ts
@@ -0,0 +1,7 @@
+let a: number = 5;
+let b: number = 6;
+let resultado: number = 0;
+
+resultado = a + b;
+
+console.log(resultado);
--- src/prueba2.ts
+++ src/prueba2.ts
@@ -1,1 +1,32 @@
-console.log("hola");
+//funcion para saber si un numero es multiplo
+function esMultiplo(dividendo: number, divisor: number): boolean {
+  if (dividendo % divisor === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Funcion para calcular la cantidad de divisores que tiene el numero ingresado por el usuario
+function cantidadDeDivisores(numeroIngresado: number): number {
+  let cantidad: number = 0;
+  let contador: number;
+  for (contador = 0; contador <= numeroIngresado; contador++) {
+    if (esMultiplo(numeroIngresado, contador) === true) {
+      cantidad++;
+    }
+  }
+  return cantidad;
+}
+
+//variable para ingresar un numero por el usuario
+let numeroIngresado: number = Number(
+  prompt("ingrese un numero para saber cuantos divisores tiene:")
+);
+console.log(
+  "el numero " +
+    numeroIngresado +
+    " tiene " +
+    cantidadDeDivisores(numeroIngresado) +
+    " divisores"
+);
